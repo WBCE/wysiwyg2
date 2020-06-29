e22d60abe435bf1b9dcc6d6f9eb53723fba7332e
instantflorian <info@meerwinck.com>
2020-06-29 17:01:14 +0200

    Update info.php

--- info.php
+++ info.php
@@ -14,14 +14,17 @@ if(count(get_included_files()) ==1){$z="HTTP/1.0 404 Not Found";header($z);die($
 $module_directory = 'wysiwyg2';
 $module_name = 'WYSIWYG2';
 $module_function = 'page';
-$module_version = '0.3';
+$module_version = '0.3.1';
 $module_platform = '1.4';
 $module_author = 'Norbert Heimsath';
 $module_license = 'WTFPL';
 $module_description = 'WYSIWYG Module whith some extra fields';
 
 /*
-0.3 2020/05/27
-! Fix for Fatal Error when used with other wysiwyg2 based modules on the same page (thx to Bernd)
-
- v0.2.1  fixed some deprecated for PHP7.4 (Bernd)
+ * 0.3.1 2020-06-22
+ * Fix Warning regarding not correct closed comment
+ * 0.3 2020-05-27
+ * Fix for Fatal Error when used with other wysiwyg2 based modules on the same page (thx to Bernd)
+ * 0.2.1
+ * Fixed some deprecated for PHP7.4 (Bernd)
+*/
